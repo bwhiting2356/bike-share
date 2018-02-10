befa860aa9b9ec7c559588d5509babe49950fd4d
Brendan Whiting <brendan.whiting@me.com>
2018-02-10 05:23:51 -0800

    small changes to map styles

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,11 +1,10 @@
-import { ApplicationRef, Component, NgZone } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicPage, ModalController, NavController, NavParams } from 'ionic-angular';
 import { GeolocationService } from '../../services/geolocation-service';
 import { LatLng } from '../../shared/LatLng';
 import 'rxjs/add/operator/startWith';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { AddressModalPage } from '../address-modal/address-modal';
-import { Observable } from 'rxjs/Observable';
 
 /**
  * Generated class for the MapPage page.
@@ -31,8 +30,6 @@ export class MapPage {
   constructor(
     private modalCtrl: ModalController,
     private geolocationService: GeolocationService,
-    private zone: NgZone,
-    private applicationRef: ApplicationRef,
     public navCtrl: NavController,
     public navParams: NavParams) {
     this.datetime = new Date().toISOString();
@@ -50,14 +47,14 @@ export class MapPage {
     const modal = this.modalCtrl.create(AddressModalPage, {title: "Origin" });
     modal.present();
     modal.onDidDismiss((choice) => {
-      this.zone.run(() => {
-        if (choice) {
-          this.origin = choice;
-          this.geolocationService.geocode(choice).then(latlng => {
-            this.originCoords.next(latlng);
-          });
-        }
-      })
+      if (choice) {
+        this.origin = choice;
+        this.geolocationService.geocode(choice).then(latlng => {
+          this.originCoords.next(latlng);
+        });
+      }
+
+      // TODO; maybe add a spinner while we're waiting for the geolocation to come back from the server
     });
   }
 
