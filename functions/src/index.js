808f3f3663790f179a87e4089c3a46cc697d301b
Brendan Whiting <brendan.whiting@me.com>
2018-02-13 01:33:27 -0800

    cleaned up functions index.js

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,22 +1,10 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const testNextDoor = require('./testNextDoor').testNextDoor;
 const findNearestStation = require('./googleMaps/findNearestStations').findNearestStations;
-const testAsyncHttp = require('./testAsyncHttp').testAsyncHttp;
 
 admin.initializeApp(functions.config().firebase);
 
-
-
-exports.helloWorldUpdate = functions.https.onRequest(function(request, response) {
-  console.log("hello world triggered");
-  response.send("Hello from Firebase updated!");
-
-});
-
-exports.testNextDoor = testNextDoor;
 exports.findNearestStation = findNearestStation;
-exports.testAsyncHttp = testAsyncHttp;
 
 
 
