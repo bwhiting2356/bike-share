1900f72e24daa53f07de09edf5da52dc6f613c22
Brendan Whiting <brendan.whiting@me.com>
2018-02-21 15:20:24 -0800

    removed trip list component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -4,15 +4,12 @@ import { TripCardComponent } from './trip-card/trip-card';
 import { PipesModule } from '../pipes/pipes.module';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from 'ionic-angular';
-import { TripListComponent } from './trip-list/trip-list';
 
 @NgModule({
 	declarations: [GoogleMapComponent,
-    TripCardComponent,
-    TripListComponent],
+    TripCardComponent],
 	imports: [CommonModule, PipesModule, IonicModule.forRoot(TripCardComponent)],
 	exports: [GoogleMapComponent,
-    TripCardComponent,
-    TripListComponent]
+    TripCardComponent]
 })
 export class ComponentsModule {}
